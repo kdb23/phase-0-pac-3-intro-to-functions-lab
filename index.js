a0a1775ec97cbf1c388e6d25a76348b21bd5c8fd
Kimberly Benton <kimberly.d.benton@gmail.com>
2023-01-10 17:41:39 -0500

    commit tree 5e723be5834c93716693574971fdf9f1c3de2e07

---
+++ index.js
@@ -0,0 +1,28 @@
+function shout(string) {
+    return string.toUpperCase();
+}
+function whisper(string) {
+    return string.toLowerCase();
+}
+function logShout(string){
+    console.log(`${string.toUpperCase()}`);
+}
+function logWhisper(string){
+    console.log(`${string.toLowerCase()}`);
+}
+const string="string";
+let response;
+
+function sayHiToHeadphonedRoommate(string){
+    if(string === string.toUpperCase()){
+        response = "YES INDEED!"
+    }
+    else if (string === string.toLowerCase()){
+        response = "I can/t hear you!"
+    }
+    else { string === string()
+        response = "I would love to!"
+    }
+     console.log(`${string}`);
+}
+sayHiToHeadphonedRoommate(string);
